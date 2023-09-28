2f4f11c8ae8d8afd886379400a86904902afffbf
Yashwant Devatwal <95556133+benjamindowns1@users.noreply.github.com>
2023-09-28 09:46:22 +0530

    main

--- database.py
+++ database.py
@@ -1,6 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy import text
-engine = create_engine("mysql+pymysql://ypuq2w686p9wa9k85e0b:pscale_pw_j9n0kto6EC8xSQCApt4ETL7Fk7YaIPe0L4zSkLHPrB6@aws.connect.psdb.cloud/juan_portfolio_website_v2?charset=utf8mb4")
+
+
+db_connect_string = "mysql+pymysql://ypuq2w686p9wa9k85e0b:pscale_pw_j9n0kto6EC8xSQCApt4ETL7Fk7YaIPe0L4zSkLHPrB6@aws.connect.psdb.cloud/juan_portfolio_website_v2?charset=utf8mb4"
+engine = create_engine(db_connect_string,connect_args={
+    "ssl":{
+        "ssl_ca":""
+    }
+})
 
 
 with engine.connect() as conn:
