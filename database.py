7a5d39e9852b42ce8db8fabf4e9f3b09e59643fd
Yashwant Devatwal <95556133+benjamindowns1@users.noreply.github.com>
2023-09-28 06:37:42 +0530

    main

--- database.py
+++ database.py
@@ -2,3 +2,5 @@ import sqlalchemy
 from sqlachemy import create_engine
 
 # print(sqlalchemy.__veresion__)
+
+engine = create_engine("mysql + pymysql://ypuq2w686p9wa9k85e0b:pscale_pw_j9n0kto6EC8xSQCApt4ETL7Fk7YaIPe0L4zSkLHPrB6@aws.connect.psdb.cloud/juan_portfolio_website_v2?charset=utf8mb4")
