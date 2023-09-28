2fa28fd3a86f92a4fd44f00a7c99a61d17535a8a
Yashwant Devatwal <95556133+benjamindowns1@users.noreply.github.com>
2023-09-28 07:13:41 +0530

    main

--- database.py
+++ database.py
@@ -1,5 +1,3 @@
 from sqlalchemy import create_engine
 
-# engine = create_engine("mysql + pymysql://ypuq2w686p9wa9k85e0b:pscale_pw_j9n0kto6EC8xSQCApt4ETL7Fk7YaIPe0L4zSkLHPrB6.@aws.connect.psdb.cloud/juan_portfolio_website_v2?charset=utf8mb4")
-
 engine = create_engine("mysql+pymysql://ypuq2w686p9wa9k85e0b:pscale_pw_j9n0kto6EC8xSQCApt4ETL7Fk7YaIPe0L4zSkLHPrB6@aws.connect.psdb.cloud/juan_portfolio_website_v2?charset=utf8mb4")
